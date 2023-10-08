f7ab947d9362389be4c7912f43bd612b8093a4d8
PrateekDe <prde1@asu.edu>
2023-10-07 22:33:49 -0700

    Recieve and sent OTP
    This is a basic Express.js app serving HTML and logging form data submitted via POST requests.

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path'); // Import the 'path' module
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json()); // Parse JSON requests
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    const htmlFilePath = path.join(__dirname, 'index.html');
+    res.sendFile(htmlFilePath);
 })
 
 app.get('/locationtracking', (req, res) => {
@@ -13,3 +19,18 @@ app.get('/locationtracking', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+app.post('/submit-form', (req, res) => {
+  const name = req.body.name;
+    console.log(name)
+    console.log(req.body)
+  const age = req.body.age;
+    console.log(age)
+    
+
+  // Do something with the form data
+  // ...
+
+  res.send('Form data submitted successfully!');
+});
+
